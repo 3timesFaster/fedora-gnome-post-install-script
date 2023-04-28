f8f2c996f12050e5e710f61df883cea8df8d4456
k1 <64timesfaster@gmail.com>
2023-04-28 18:34:32 -0400

    Added RPM fuision, gnome-extensions-manager, put dnf remove before update

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -25,9 +25,6 @@ else
     echo "fastmirror has been set to True"
 fi
 
-# update
-dnf update -y
-
 # remove junk from workstation
 dnf remove -y gnome-connections \
 	anaconda \
@@ -44,5 +41,35 @@ dnf remove -y gnome-connections \
 	cheese \
 	rhythmbox \
 	totem \
-	libreoffice-core
+	libreoffice-core \
+	gnome-boxes
+	
+# Dnf firmware and multimedia 
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+       	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+	rpmfusion-nonfree-release-tainted \
+	gnome-tweaks
+dnf groupupdate core \
+	multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin \
+	sound-and-video
+dnf --repo=rpmfusion-nonfree-tainted install "*-firmware"
 
+# Gnome extensions
+flatpak install flathub com.mattjakeman.ExtensionManager 
+
+# update
+dnf update -y
+
+# Check if Intel integrated graphics card is present
+if lspci -vnn | grep VGA | grep -iq "intel"; then
+    echo "Intel integrated graphics detected. Installing intel-media-driver."
+    sudo dnf install intel-media-driver
+# Check if Nvidia graphics card is present
+elif lspci -vnn | grep VGA | grep -iq "nvidia"; then
+    echo "Nvidia graphics card detected. Installing nvidia-vaapi-driver."
+    sudo dnf install nvidia-vaapi-driver
+    echo "Remember to use the Howto/Nvidia page on the RPM fuision site to install the proprietary drivers"
+    echo "https://rpmfusion.org/Howto/NVIDIA?highlight=%28%5CbCategoryHowto%5Cb%29"
+else
+    echo "No supported graphics card detected. or its an AMD which means your good."
+fi
